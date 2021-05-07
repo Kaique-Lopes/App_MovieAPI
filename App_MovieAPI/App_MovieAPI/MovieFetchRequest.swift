f45ddf61f159400d0238c0105b3c41ff462b0178
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-05-06 21:23:52 -0300

    URLSession

---
+++ App_MovieAPI/App_MovieAPI/MovieFetchRequest.swift
@@ -0,0 +1,13 @@
+//
+//  MovieFetchRequest.swift
+//  App_MovieAPI
+//
+//  Created by Kaique Lopes on 06/05/21.
+//
+
+import Foundation
+
+class REST {
+    
+    let url = URL(string: "https://api.themoviedb.org/3/movie/550?api_key=7f83f0a91411cb45647d15d4ab954ae5")
+}
--- App_MovieAPI/App_MovieAPI/ViewController.swift
+++ App_MovieAPI/App_MovieAPI/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let url = URL(string: "https://api.themoviedb.org/3/movie/550?api_key=7f83f0a91411cb45647d15d4ab954ae5") {
+            let data = URLSession.shared.dataTask(with: url) { datas, request, error in
+                if error == nil {
+                    print("Sucesso ao consultar Filmes!")
+                } else {
+                    print("Houve algum erro ao consultar os Filmes!")
+                }
+            }
+            data.resume()
+        }
+        
+        
     }
-
-
+    
+    
 }
 
